bc6bc066b20093223f150a628f0afe71a4c72ba4
BYU Rental <josh.a.wiseman@gmail.com>
2023-04-05 22:52:04 -0600

    8:30 hours, Stressed out over who could access the github stuff, made sickness a thing, clarified some more, fixed some cautions, and also did the one time 100s, cap on number of times you can go eventless

--- src/YearCycler.java
+++ src/YearCycler.java
@@ -1,10 +1,14 @@
 import java.util.Random;
-import java.util.Scanner;
 
+
+/**
+ * This will cycle through the years, make sure that they are valid years
+ * cause events, and tell you info about the current year
+ */
 public class YearCycler {
     Interactions I = new Interactions();
     Random myRandom = new Random();
-    Scanner theScanner = new Scanner(System.in);
+    //Scanner theScanner = new Scanner(System.in);
 
     public void reelingInTheYears() {
 
@@ -15,30 +19,60 @@ public class YearCycler {
                 reelingInTheYears();
             }
             else {
+                //Prophet moment
+                if(EData.isInQuorumOf12){
+                    EData.faith+=5;
+                }
+
+                moneyAndFaithCheck();
+
                 //Event Time, before info on the year
                 int randomInt = myRandom.nextInt(0,100);
-                if (randomInt>=60){ //40% chance of event occurring, lowering to make the game more informative
+                if (randomInt>=60 || EData.turnsSinceLastEvent >=3){ //40% chance of event occurring, lowering to make the game more informative
                     generalEvents event = new generalEvents();
+                    event.generalEventsStart();
+                }
+                else {
+                    EData.turnsSinceLastEvent+=1;
                 }
 
                 //Info time!
                 info = "In " + EData.currentYear + ": " + EData.hashMap.get(EData.currentYear);
                 I.out(info);
 
-
-
             }
         }
         else {
-            assert EData.currentYear > 1846;
-            info = "You have finished the game!";
+            //assert EData.currentYear > 1846;
+            I.out("You have finished the game!");
+            if(EData.faith >=100){
+                I.out("With immense faith at that!");
+            }
+            if(EData.faith >=100){
+                I.out("With immense money at that!");
+            }
+            System.exit(0);
         }
         System.out.flush();
 
     }
 
-    public void actionEvent() {
-
+    //Checking to see what should be done with certain levels of money and faith
+    private void moneyAndFaithCheck() {
+        if(EData.money<=0){
+            I.out("You have no money left and have starved! Try holding on to it better next time");
+            System.exit(0);
+        }
+        else if(EData.faith<=0){
+            I.out("You have no faith left in the church and have been kicked! Try being more faithful next time");
+            System.exit(0);
+        }
+        else if (EData.money>=100 && !EData.claimed100Money) {
+            generalEvents.Money100Event();
+        }
+        else if (EData.faith>=100 && !EData.claimed100Faith) {
+            generalEvents.Faith100Event();
+        }
     }
 
 
--- src/generalEvents.java
+++ src/generalEvents.java
@@ -1,31 +1,94 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This is where all the events are
+ */
 public class generalEvents {
-    Interactions I = new Interactions();
-    Scanner myScanner = new Scanner(System.in);
+    static Interactions I = new Interactions();
+    static Scanner myScanner = new Scanner(System.in);
     Random myRand = new Random();
 
     public generalEvents(){
-        if(EData.money<=0){
-            I.out("You have no money left and have starved! Try holding on to it better next time");
-            System.exit(0);
-        }
-        else if(EData.faith<=0){
-            I.out("You have no faith left in the church and have been kicked! Try being more faithful next time");
-            System.exit(0);
-        }
 
-        int randomInt = myRand.nextInt(0,100);
+    }
 
+    public void generalEventsStart(){
+        EData.turnsSinceLastEvent = 0;
+
+        int randomInt = myRand.nextInt(0,100); //Number to help randomize events
+
+        //Bog event before all else if the time is right
         if(EData.currentYear==1838){
             event1838();
         }
-        else if(randomInt% EData.money <7){ //Decreased it to reduce it
-            eventMoney1();
+        /*My brain can't figure out the correct math for this
+        Random int has a straight change of 7% to be broke
+        At 14 faith you have a 50 50 chance?*/
+        else if(randomInt% EData.money <8){ //Decreased it to reduce it
+            //Coin-flip which money it is
+            if (myRand.nextBoolean()){
+                eventMoney2();
+            }
+            else {
+                eventMoney1();
+            }
         }
         else {
-            getRandomEvent();
+            getRandomCommonEvent();
+        }
+    }
+
+    //Events when you get 100 for the first time
+    public static void Faith100Event() {
+        EData.claimed100Faith = true;
+        I.out("Your faith is exceeding strong and you receive a choice");
+        I.conditionalSleep(1000);
+        I.out("Join the quorum of the 12 (A), or ask God for a miracle of wealth? (B)");
+        String input = I.in(myScanner);
+        if(input.compareToIgnoreCase("A") == 0){
+            I.out("You join the quorum of the 12, your faith will go up every turn");
+            I.conditionalSleep(1000);
+            I.out("In reality, the quorum wasn't established until 1835, and it succession is based upon death");
+            EData.isInQuorumOf12 = true;
+            I.conditionalSleep(1000);
+        }
+        else if(input.compareToIgnoreCase("B") == 0){
+            I.out("And God blesses you and your family with wealth");
+            if( EData.money <30){
+                I.out("Blessed are the meek and lowly of less than 30 money that they may inherit money... Wait a minute, that's not how it goes...");
+                EData.money*=2;
+            }
+            else {
+                I.out("You receive an outpouring of earthly blessings from heaven. Now don't get too greedy alright?");
+            }
+            EData.money+=25;
+        }
+        else {
+            I.out("Not a valid input, try again");
+            Faith100Event();
+        }
+    }
+    public static void Money100Event() {
+        EData.claimed100Money = true;
+        I.out("Holy smokes you got a lot of money!!!");
+        I.conditionalSleep(1000);
+        I.out("Contribute some of it to increase your faith? (Y/N)");
+        String input = I.in(myScanner);
+        I.conditionalSleep(1000);
+        if(input.compareToIgnoreCase("Y") == 0){
+            I.out("Some money is donated to the church");
+            EData.money-=25;
+            EData.faith+=45;
+            I.conditionalSleep(1000);
+        }
+        else if(input.compareToIgnoreCase("N") == 0){
+            I.out("Well... your money is safe with you");
+            I.conditionalSleep(1000);
+        }
+        else {
+            I.out("Not a valid input, try again");
+            Money100Event();
         }
     }
 
@@ -180,7 +243,7 @@ public class generalEvents {
         else if (response.compareToIgnoreCase("b")==0) {
 
             I.out("You pray to God and hope that your faith is enough");
-            int testDaFaith = myRand.nextInt(0,75);
+            int testDaFaith = myRand.nextInt(0,76);
             int faith =  testDaFaith/ EData.faith; //Bump up 75 to make it event harder
             if(faith == 0){
                 I.out("Your faith was not enough... you find someone willing to sell you food but at exorbitant prices");
@@ -201,6 +264,59 @@ public class generalEvents {
             I.out("I don't know what you were thinking with this one, but the sweet release of death washes over you as you ascend to heaven");
             System.exit(0);
 
+        }
+        else {
+            I.out("Not a valid response, lets try that again");
+            eventGeneral1();
+        }
+    }
+    public void eventMoney2(){
+        I.out("Event: The bishop notices that you are broke and starving:");
+        I.out("A: Let him contribute some alms to you");
+        I.out("B: Pray to God for food and money with your bishop");
+        I.out("C: Be prideful and pay for it yourself");
+        I.out("D: Just die");
+        String response = I.in(myScanner);
+
+        if (response.compareToIgnoreCase("a")==0){
+            I.out("The bishop goes around the community to help gather some money for you");
+            EData.money+=10;
+            EData.faith+=10;
+            I.conditionalSleep(1000);
+            I.out("You receive some money and your faith in your bishop is improved");
+            I.conditionalSleep(1000);
+
+        }
+        else if (response.compareToIgnoreCase("b")==0) {
+
+            I.out("You pray to God with your bishop and hope that your combined faith is enough");
+            int testDaFaith = myRand.nextInt(0,31); // your faith will have to be 15 or higher on average, so easier
+            int faith =  testDaFaith/ EData.faith;
+            if(faith == 0){
+                I.out("Your faith was not enough... you find someone willing to sell you food but at exorbitant prices");
+                I.conditionalSleep(1000);
+                EData.money-=30;
+            }
+            else {
+                I.out("God has prepared a way for you! Food has been secured. It really was easier with your bishop there");
+                if(EData.faith/2 > testDaFaith){
+                    I.out("In fact, you were so faithful that God helped you procure some extra funds as well!!");
+                    I.conditionalSleep(1000);
+                    EData.money+=30;
+                }
+            }
+
+        }
+        else if (response.compareToIgnoreCase("c")==0) {
+            I.out("Food has been paid for, but you notice you become even poorer");
+            EData.money-=15;
+            I.conditionalSleep(1000);
+            I.out("In reality, 100's of saints starved while fleeing West, and had no opportunity to purchase food");
+            I.conditionalSleep(1000);
+        }
+        else if (response.compareToIgnoreCase("d")==0) {
+            I.out("I don't know what you were thinking with this one, but the sweet release of death washes over you as you ascend to heaven");
+            System.exit(0);
         }
         else {
             I.out("Not a valid response, lets try that again");
@@ -265,8 +381,9 @@ public class generalEvents {
 
     }
 
-    public void getRandomEvent(){
-        int rand = myRand.nextInt(0,3);
+    public void getRandomCommonEvent(){
+        //I think it doesn't actually include the bound, so bound-1 is max
+        int rand = myRand.nextInt(0,4);
         if (rand==0){
             eventGeneral1(); //Bishop One
         }
